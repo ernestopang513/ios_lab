0b2c89cd0edf85b092830d46f44341c123c3772e
MCErnesto <ernestopang513@hotmail.com>
2024-09-04 15:18:05 -0600

    Desafio de SwiftUI

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = 0
     @State private var tipPercentage = 0
     @FocusState private var amountIsFocused: Bool
     let tipPercentages = [0, 10, 15, 20, 25]
     
     var totalPerPerson: Double {
         
-        let peopleCount = Double(numberOfPeople+2)
+        let peopleCount = Double(numberOfPeople+1)
         let tipSelection = Double(tipPercentage)
         
         let tipValue = checkAmount / 100 * tipSelection
@@ -25,6 +25,16 @@ struct ContentView: View {
         
         return amountPerPerson
         
+    }
+    
+    var totalAmountWithTip: Double {
+        
+        let tipSelection = Double(tipPercentage)
+
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        
+        return grandTotal
     }
     
     
@@ -42,16 +52,19 @@ struct ContentView: View {
                     }
                     .pickerStyle(.navigationLink)
                 }
-                Section{
-                    Text("How much do you want to tip?")
+                Section("How much do you want to tip?"){
+//                    Text("How much do you want to tip?")
                     Picker("Tip percentage", selection: $tipPercentage){
                         ForEach(tipPercentages, id: \.self){
                             Text($0, format: .percent)
                         }
-                                }.pickerStyle(.segmented)
+                                }.pickerStyle(.navigationLink)
                 }
-                Section{
+                Section("Amount per person"){
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                Section("Total amount"){
+                    Text(totalAmountWithTip, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
             .navigationTitle("WeSplit")
