bb4005d5ee717a9727517e0bf56220621ba7a9de
MCErnesto <ernestopang513@hotmail.com>
2024-09-09 17:21:46 -0600

    GuessTheFlag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ernesto Pang on 09/09/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
