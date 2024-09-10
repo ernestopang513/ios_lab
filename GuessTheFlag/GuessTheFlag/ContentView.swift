a2bf64af878abcf5f7296c9e5c2b5811710f2038
MCErnesto <ernestopang513@hotmail.com>
2024-09-10 17:24:56 -0600

    Agregue mas estilos a guess the flag

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -25,31 +25,44 @@ struct ContentView: View {
             RadialGradient(stops: [.init(color: .orange, location: 0.3),
                                    .init(color: .cyan, location: 0.3)
             ], center: .top, startRadius: 200, endRadius: 700).ignoresSafeArea()
-//            LinearGradient(colors: [.white, .blue], startPoint: .top, endPoint: .bottom)
-//                .ignoresSafeArea()
+            //            LinearGradient(colors: [.white, .blue], startPoint: .top, endPoint: .bottom)
+            //                .ignoresSafeArea()
             
             
-//            Color.blue.ignoresSafeArea()
+            //            Color.blue.ignoresSafeArea()
             
-            VStack (spacing: 30) {
-                VStack {
-                    Text("Tap the flag of").foregroundStyle(.white)
-//                        .font(.subheadline.weight(.heavy))
-                        .font(.system(size: 20, weight: .heavy))
-                    Text(countries[correctAnswer]).foregroundStyle(.white)
-//                        .font(.subheadline.weight(.semibold))
-                        .font(.system(size: 20, weight: .none))
-                }
-                ForEach(0..<3){ number in
-                    Button {
-                        flagTapped(number)
-                    } label : {
-                        Image(countries[number])
-                            .clipShape(.capsule)
-                            .shadow(radius: 5)
-////                        Text("Hola")
+            VStack{
+                Text("Guess the Flag").foregroundStyle(.white)
+                    .font(.system(size: 30, weight: .heavy))
+                
+                VStack (spacing: 30) {
+                    VStack {
+                        Text("Tap the flag of").foregroundStyle(.black)
+                        //                        .font(.subheadline.weight(.heavy))
+                            .font(.system(size: 20, weight: .heavy))
+                        Text(countries[correctAnswer]).foregroundStyle(.black)
+                        //                        .font(.subheadline.weight(.semibold))
+                            .font(.system(size: 20, weight: .none))
+                    }
+                    ForEach(0..<3){ number in
+                        Button {
+                            flagTapped(number)
+                        } label : {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                            ////                        Text("Hola")
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 25)
+                .background(.thinMaterial)
+                .clipShape(.rect(cornerRadius: 40))
+                
+                Text("Score: ??")
+                    .foregroundStyle(.white)
+                    .font(.system(size: 30, weight: .semibold))
             }
         }
         .alert(scoreTitle, isPresented: $showingScore){
