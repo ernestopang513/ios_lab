b38d37b72279f6ecaac80bd99291c68b615f3bbd
MCErnesto <ernestopang513@hotmail.com>
2024-09-18 12:53:20 -0600

    BetterRest VStack y formato

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ernesto Pang on 18/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -23,18 +23,13 @@ struct ContentView: View {
     
     var body: some View {
         ZStack{
-            
             RadialGradient(stops: [.init(color: .orange, location: 0.3),
                                    .init(color: .cyan, location: 0.3)
             ], center: .top, startRadius: 200, endRadius: 700).ignoresSafeArea()
             //            LinearGradient(colors: [.white, .blue], startPoint: .top, endPoint: .bottom)
             //                .ignoresSafeArea()
-            
-            
             //            Color.blue.ignoresSafeArea()
-            
             VStack{
-                
                 Text("Guess the Flag").foregroundStyle(.white)
                     .font(.system(size: 30, weight: .heavy))
                 
@@ -58,20 +53,21 @@ struct ContentView: View {
                         }
                     }
                 }
+//               El frame de abajo ajusta el frame del VStack donde se encuentra el fondo
+//                de thinMaterial donde el ancho esta ajustado a tomar todo el tamaño posible
                 .frame(maxWidth: .infinity)
+//                El sisguiente padding me da el marco de 
                 .padding(.vertical, 25)
                 .background(.thinMaterial)
                 .clipShape(.rect(cornerRadius: 40))
-                
                 Spacer()
-                
-                
                 Text("Score: \(score)")
                     .foregroundStyle(.white)
                     .font(.system(size: 30, weight: .semibold))
                 Spacer()
                 Button {
                     score = 0
+                    countGame = 0
                 } label: {
                     Text("Reset").padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
                         .foregroundColor(.white).font(.system(size: 30, weight: .semibold))
@@ -83,34 +79,41 @@ struct ContentView: View {
         }
         .alert(scoreTitle, isPresented: $showingScore){
             Button("Continue", action: {
-                
                 askQuiestion()
                 GameReset()
-                
             })
         } message: {
-            Text("Your score is \(score)")
+                Text("Your score is \(score)")
         }
     }
     func flagTapped(_ number: Int){
-        if number == correctAnswer {
-            scoreTitle = "Correct"
-            score += 1
-        }else {
-            scoreTitle = "Wrong! That´s the flag of \(countries[correctAnswer])"
-            if score > 0 {
-                score -= 1
+        countGame += 1
+            if number == correctAnswer {
+                score += 1
+                if countGame == 8 {
+                    scoreTitle = "Thanks for playing"
+                }else {
+                    scoreTitle = "Correct"
+                }
+            }else  {
+                
+                if countGame == 8 {
+                    scoreTitle = "Thanks for playing"
+                }else{
+                    scoreTitle = "Wrong! That´s the flag of \(countries[number])"
+                }
             }
-        }
         showingScore = true
     }
     func askQuiestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
-        countGame += 1
     }
     func GameReset(){
-        
+        if countGame == 8 {
+            score = 0
+            countGame = 0
+        }
     }
 }
 
