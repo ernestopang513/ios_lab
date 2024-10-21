5c45a3bb35973ade1d87c89b419c746a712b3fa9
MCErnesto <ernestopang513@hotmail.com>
2024-10-21 16:16:14 -0600

    IExpensive tiene ventana extra
    Este commit es importante por que sirve para la app de challenge

---
+++ iExpensive/iExpensive/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  iExpensive
+//
+//  Created by Ernesto Pang on 30/09/24.
+//
+
+import SwiftUI
+
+struct ExpensesItem : Identifiable{
+    let id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+     var items = [ExpensesItem]()
+}
+
+
+
+struct ContentView: View {
+    
+    @State private var expenses = Expenses()
+    
+    @State private var showingAddExpense = false
+    
+    var body: some View {
+        NavigationStack{
+            List {
+                ForEach(expenses.items){
+                    item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems )
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus"){
+                    showingAddExpense = true}
+                }
+            }
+            .sheet(isPresented: $showingAddExpense){
+                AddView(expenses: expenses)
+            }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iExpensive/iExpensive/iExpensiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpensiveApp.swift
+//  iExpensive
+//
+//  Created by Ernesto Pang on 30/09/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpensiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
